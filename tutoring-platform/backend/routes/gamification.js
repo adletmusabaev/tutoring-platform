00dc26cc6f6eb1885b36c32f8305548c45b98b2d
adletmusabaev <60289832+adletmusabaev@users.noreply.github.com>
2026-05-20 20:37:35 +0500

    Delete tutoring-platform directory

--- tutoring-platform/backend/routes/gamification.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/auth');
-const { getMyGamification, getLeaderboard } = require('../controllers/gamificationController');
-
-router.get('/me', authenticateToken, getMyGamification);
-router.get('/leaderboard', authenticateToken, getLeaderboard);
-
-module.exports = router;
